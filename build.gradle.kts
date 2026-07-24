e2cc72fca4e664e1bef2c49d8c7e4c7c97117caa
Jeong Grius <yuchan8003@naver.com>
2026-07-24 18:53:04 +0900

    Chore: 인스턴스 API 데모 도구 추가
    http/instances-demo.http: IntelliJ HTTP Client용 재현 스크립트
    static/demo.html: 생성·조회·삭제를 브라우저에서 확인하는 대시보드

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	implementation("tools.jackson.module:jackson-module-kotlin")
+	developmentOnly("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.0")
 	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-actuator-test")
